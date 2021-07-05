acccb34957ed3f2156fee715bbe838e43fd0298e
Karma Blackshaw <irenesejah29@gmail.com>
2021-07-05 15:36:53 +0800

    update: lint

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:vue/essential',
-    '@vue/standard',
     'plugin:vue/recommended'
   ],
 
--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,24 +1,35 @@
 <template>
-  <v-app id="inspire" >
-    <v-navigation-drawer v-model="drawer" app      
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
       fab
       dark
       large
-      color="blue darken-1">
+      color="blue darken-1"
+    >
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="text-h6"> MP3 Player </v-list-item-title>
+          <v-list-item-title class="text-h6">
+            MP3 Player
+          </v-list-item-title>
           <v-list-item-subtitle> Colin Ripalda </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
-      <v-divider></v-divider>
+      <v-divider />
 
-      <v-list dense nav subheader>
-        <v-subheader inset>Songs</v-subheader>
+      <v-list
+        dense
+        nav
+        subheader
+      >
+        <v-subheader inset>
+          Songs
+        </v-subheader>
 
         <v-list-item-group>
-          <v-list-item @click="modals.addSong.status = 1" color="primary">
+          <v-list-item @click="modals.addSong.status = 1">
             <v-list-item-icon>
               <v-icon> mdi-music-note-plus </v-icon>
             </v-list-item-icon>
@@ -39,12 +50,14 @@
           </v-list-item>
         </v-list-item-group>
 
-        <v-divider></v-divider>
+        <v-divider />
 
-        <v-subheader inset>Playlist</v-subheader>
+        <v-subheader inset>
+          Playlist
+        </v-subheader>
 
         <v-list-item-group>
-          <v-list-item @click="modals.addPlaylist.status = 1" color="primary">
+          <v-list-item @click="modals.addPlaylist.status = 1">
             <v-list-item-icon>
               <v-icon> mdi-playlist-music </v-icon>
             </v-list-item-icon>
@@ -54,43 +67,65 @@
             </v-list-item-content>
           </v-list-item>
 
-          <v-list-item v-for="(currPlaylist, playlistKey) in playlists" :key="playlistKey">
+          <v-list-item
+            v-for="(currPlaylist, playlistKey) in playlists"
+            :key="playlistKey"
+          >
             <v-list-item-icon class="ml-5">
-              <v-icon small>mdi-folder-music</v-icon>
+              <v-icon small>
+                mdi-folder-music
+              </v-icon>
             </v-list-item-icon>
 
             <v-list-item-content>
-              <v-list-item-title>{{currPlaylist.name}}</v-list-item-title>
+              <v-list-item-title>{{ currPlaylist.name }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app flat color="white">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar
+      app
+      flat
+      color="white"
+    >
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
 
       <v-toolbar-title>ALL SONGS</v-toolbar-title>
     </v-app-bar>
 
-    <v-main >
+    <v-main>
       <v-container fluid>
         <div>
-          <v-simple-table  
+          <v-simple-table
             fab
             large
-            color="blue darken-1">>
+            color="blue darken-1"
+          >
+            >
             <template v-slot:default>
               <thead>
                 <tr>
-                  <th class="text-left">Title</th>
-                  <th class="text-left">Artist</th>
-                  <th class="text-left">Album</th>
-                  <th class="text-left">Duration</th>
+                  <th class="text-left">
+                    Title
+                  </th>
+                  <th class="text-left">
+                    Artist
+                  </th>
+                  <th class="text-left">
+                    Album
+                  </th>
+                  <th class="text-left">
+                    Duration
+                  </th>
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="(item, itemKey) in songs" :key="item.name">
+                <tr
+                  v-for="(item, itemKey) in songs"
+                  :key="itemKey"
+                >
                   <td>{{ item.title }}</td>
                   <td>{{ item.artist }}</td>
                   <td>{{ item.album }}</td>
@@ -103,25 +138,36 @@
       </v-container>
     </v-main>
 
-    <v-footer padless inset app>
-      <v-col cols="12" class="pa-0">
-        <v-card flat color="transparent" class="pa-0 ma-0">
+    <v-footer
+      padless
+      inset
+      app
+    >
+      <v-col
+        cols="12"
+        class="pa-0"
+      >
+        <v-card
+          flat
+          color="transparent"
+          class="pa-0 ma-0"
+        >
           <v-progress-linear
             :value="50"
             class="my-0"
             height="3"
-          ></v-progress-linear>
+          />
 
           <v-list>
             <v-list-item>
               <v-list-item-content>
                 <v-list-item-title>The Walker</v-list-item-title>
-                <v-list-item-subtitle
-                  >Fitz & The Trantrums</v-list-item-subtitle
-                >
+                <v-list-item-subtitle>
+                  Fitz & The Trantrums
+                </v-list-item-subtitle>
               </v-list-item-content>
 
-              <v-spacer></v-spacer>
+              <v-spacer />
 
               <v-list-item-icon>
                 <v-btn icon>
@@ -151,90 +197,117 @@
       </v-col>
     </v-footer>
 
-    <v-dialog v-model="modals.addSong.status" width="500">
+    <v-dialog
+      v-model="modals.addSong.status"
+      width="500"
+    >
       <v-card>
-        <v-card-title class="text-h5 grey lighten-2"> Add Song </v-card-title>
+        <v-card-title class="text-h5 grey lighten-2">
+          Add Song
+        </v-card-title>
 
         <v-card-text>
           <v-text-field
             v-model="modals.addSong.data.title"
             label="Title"
-          ></v-text-field>
+          />
           <v-text-field
             v-model="modals.addSong.data.artist"
             label="Artist"
-          ></v-text-field>
+          />
           <v-text-field
             v-model="modals.addSong.data.album"
             label="Album"
-          ></v-text-field>
+          />
           <v-text-field
             v-model="modals.addSong.data.duration"
             label="Duration"
-          ></v-text-field>
+          />
         </v-card-text>
 
-        <v-divider></v-divider>
+        <v-divider />
 
         <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="handleClickCreateSong">
+          <v-spacer />
+          <v-btn
+            color="primary"
+            text
+            @click="handleClickCreateSong"
+          >
             Create
           </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
-    <v-dialog v-model="modals.editSong.status" width="500">
+    <v-dialog
+      v-model="modals.editSong.status"
+      width="500"
+    >
       <v-card>
-        <v-card-title class="text-h5 grey lighten-2"> Add Song </v-card-title>
+        <v-card-title class="text-h5 grey lighten-2">
+          Add Song
+        </v-card-title>
 
         <v-card-text>
           <v-text-field
             v-model="modals.editSong.data.title"
             label="Title"
-          ></v-text-field>
+          />
           <v-text-field
             v-model="modals.editSong.data.artist"
             label="Artist"
-          ></v-text-field>
+          />
           <v-text-field
             v-model="modals.editSong.data.album"
             label="Album"
-          ></v-text-field>
+          />
           <v-text-field
             v-model="modals.editSong.data.duration"
             label="Duration"
-          ></v-text-field>
+          />
         </v-card-text>
 
-        <v-divider></v-divider>
+        <v-divider />
 
         <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="handleClickEditSong">
+          <v-spacer />
+          <v-btn
+            color="primary"
+            text
+            @click="handleClickEditSong"
+          >
             Edit
           </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
-    <v-dialog v-model="modals.addPlaylist.status" width="500">
+    <v-dialog
+      v-model="modals.addPlaylist.status"
+      width="500"
+    >
       <v-card>
-        <v-card-title class="text-h5 grey lighten-2"> Create Playlist </v-card-title>
+        <v-card-title class="text-h5 grey lighten-2">
+          Create Playlist
+        </v-card-title>
 
         <v-card-text>
           <v-text-field
             v-model="modals.addPlaylist.data.name"
             label="Name"
-          ></v-text-field>
+          />
         </v-card-text>
 
-        <v-divider></v-divider>
+        <v-divider />
 
         <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="handleClickCreatePlaylist">
+          <v-spacer />
+          <v-btn
+            color="primary"
+            text
+            @click="handleClickCreatePlaylist"
+          >
             Create
           </v-btn>
         </v-card-actions>
@@ -255,7 +328,7 @@ export default {
           title: null,
           artist: null,
           album: null,
-          duration: null,
+          duration: null
         },
         reset() {
           this.status = 0;
@@ -263,9 +336,9 @@ export default {
             title: null,
             artist: null,
             album: null,
-            duration: null,
+            duration: null
           };
-        },
+        }
       },
 
       editSong: {
@@ -275,7 +348,7 @@ export default {
           artist: null,
           album: null,
           duration: null,
-          index: null,
+          index: null
         },
         reset() {
           this.status = 0;
@@ -284,28 +357,28 @@ export default {
             artist: null,
             album: null,
             duration: null,
-            index: null,
+            index: null
           };
-        },
+        }
       },
 
       addPlaylist: {
         status: 0,
         data: {
-          name: null,
+          name: null
         },
         reset() {
           this.status = 0;
           this.data = {
-            name: null,
+            name: null
           };
-        },
-      },
+        }
+      }
     },
 
     songs: [],
 
-    playlists: [],
+    playlists: []
   }),
 
   methods: {
@@ -323,7 +396,7 @@ export default {
       modal.status = 1;
       modal.data = {
         ...item,
-        index,
+        index
       };
     },
 
@@ -348,6 +421,6 @@ export default {
 
       modal.reset()
     }
-  },
+  }
 };
 </script>
